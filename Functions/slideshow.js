457b193c2ce2968bebb4fcbb543f62b031bef47c
Guven <mukremin@live.co.uk>
2023-06-09 17:48:12 +0100

    Update to HTML, JS and CSS
    Added Slideshow and new images
    Updated Game Developer Section

--- Functions/slideshow.js
+++ Functions/slideshow.js
@@ -1,21 +1,40 @@
 window.onload = function() {
-    
-let myIndex = 0;
-showSlides();
 
-function showSlides() {
-  var i;
-  var x = document.getElementsByClassName("Slideshow");
-  if (x.length === 0) {
-    console.error('No elements found with class name "Slideshow"');
-    return;
+  let myIndex = 0;
+  showSlides();
+  function showSlides() {
+      var i;
+      var x = document.getElementsByClassName("Slideshow");
+      if (x.length === 0) {
+          console.error('No elements found with class name "Slideshow"');
+          return;
+      }
+      for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
+      }
+      if (myIndex >= x.length) {myIndex = 0}
+      x[myIndex].style.display = 'block';
+      myIndex++;
+      setTimeout(showSlides, 4000); // Change image every 4 seconds
   }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+
+  var slideIndex2 = 0;
+  showSlides2();
+  function showSlides2() {
+      var i;
+      var slides = document.getElementsByClassName("Slideshow2");
+      for (i = 0; i < slides.length; i++) {
+          slides[i].classList.add("slide-right"); // Add "slide-right" class for slide to right effect
+          slides[i].classList.add("zoom-in"); // Add "zoom-in" class for zoom in effect
+          slides[i].style.display = "none";
+      }
+
+      slideIndex2++;
+      if (slideIndex2 > slides.length) {
+          slideIndex2 = 1
+      }
+
+      slides[slideIndex2 - 1].style.display = "block";
+      setTimeout(showSlides2, 4000); // Change image every 4 seconds
   }
-  if (myIndex >= x.length) {myIndex = 0}    
-  x[myIndex].style.display = 'block';  
-  myIndex++;
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
 }
---
+++ Functions/slideshow2.js
@@ -0,0 +1,23 @@
+// window.onload = function() {
+    
+//     let myIndex = 0;
+//     showSlides2();
+    
+//     function showSlides2() {
+//       var i;
+//       var x = document.getElementsByClassName("Slideshow2");
+//       if (x.length === 0) {
+//         console.error('No elements found with class name "Slideshow2"');
+//         return;
+//       }
+//       for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";  
+//       }
+//       if (myIndex >= x.length) {myIndex = 0}    
+//       x[myIndex].style.display = 'block';  
+//       myIndex++;
+//       setTimeout(showSlides2, 5000); // Change image every 4 seconds
+//     }
+
+    
+//     }
