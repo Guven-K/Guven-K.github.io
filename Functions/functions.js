644b08f99dccb0518ce53cece31494500fb3fcb7
Guven <mukremin@live.co.uk>
2023-07-03 13:17:52 +0100

    Another update to HTML, CSS and JS

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,12 +1,25 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("NavBar").style.top = "0";
-  } else {
-    document.getElementById("NavBar").style.top = "-120px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+document.addEventListener("DOMContentLoaded", function() {
+  var prevScrollpos = window.pageYOffset;
+  var navBar = document.getElementById("NavBar");
 
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      navBar.style.top = "0";
+    } else {
+      navBar.style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+  };
 
+  navBar.onmouseenter = function() {
+    navBar.style.top = "0";
+  };
+
+  navBar.onmouseleave = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos < currentScrollPos) {
+      navBar.style.top = "-150px";
+    }
+  };
+});
