39804d4c6851445d3d7ec1b3ff5d19432c2dff43
Guven <mukremin@live.co.uk>
2024-12-29 00:20:29 +0000

    CodeGPT

--- Functions/cursor.js
+++ Functions/cursor.js
@@ -4,3 +4,14 @@ setInterval(() => {
   const cursor = document.querySelector('.cursor');
   cursor.style.visibility = cursor.style.visibility === 'hidden' ? 'visible' : 'hidden';
 }, 500);
+
+
+
+const myObject = {
+  myMethod() {
+    let person = 'Maybe consider buying me a coffee if you are impressed with my work. :)';
+    console.log(person);
+  }
+};
+
+myObject.myMethod();
