0bb79a2e24f9dc3d39388a2d2f8bffc857cc1cf8
Guven <mukremin@live.co.uk>
2025-04-15 21:26:56 +0100

    Navbar imporvements, auto scrol and mouse hover top window to unhide navbar

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,25 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var prevScrollpos = window.pageYOffset;
-  var navBar = document.getElementById("NavBar");
+document.addEventListener("DOMContentLoaded", function () {
+  const navBar = document.getElementById("NavBar");
+  const trigger = document.getElementById("ScrollTrigger");
 
-  window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      navBar.style.top = "0";
+  let scrollThreshold = trigger.offsetTop;
+
+  window.addEventListener("resize", () => {
+    scrollThreshold = trigger.offsetTop;
+  });
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > scrollThreshold) {
+      navBar.style.top = "-150px";
     } else {
+      navBar.style.top = "0";
+    }
+  });
+
+  document.addEventListener("mousemove", (e) => {
+    if (e.clientY <= 100) {
+      navBar.style.top = "0";
+    }
+  });
+
+  navBar.addEventListener("mouseleave", () => {
+    if (window.pageYOffset > scrollThreshold) {
       navBar.style.top = "-150px";
     }
-    prevScrollpos = currentScrollPos;
-  };
-
-  navBar.onmouseenter = function() {
-    navBar.style.top = "0";
-  };
-
-  navBar.onmouseleave = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos < currentScrollPos) {
-      navBar.style.top = "-150px";
-    }
-  };
+  });
 });
